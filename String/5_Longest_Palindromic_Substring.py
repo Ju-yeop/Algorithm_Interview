8551ff2510e2181726e30c5ee2300658f81ea73f
Ju-yeop <dhwnduq12@naver.com>
2022-08-18 19:07:57 +0900

    22.08.18

--- String/5_Longest_Palindromic_Substring.py
+++ String/5_Longest_Palindromic_Substring.py
@@ -1,3 +1,24 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        longest_str = ""
+        length = len(s)
+        for i in range(length):
+            sub_longest_str = s[i]
+            for j in range(length - i):
+                curr = s[i:length - j]
+                if curr == curr[::-1] and len(sub_longest_str) < len(curr):
+                    sub_longest_str = curr
+                    break
+
+            if len(longest_str) < len(sub_longest_str):
+                longest_str = sub_longest_str
+
+            if len(longest_str) >= length - i - 1:
+                break
+
+        return longest_str
+
+
+test = Solution()
+print(test.longestPalindrome("babad"))
