31850ea684b539f5332a259c1ef4f3a47725456f
Ju-yeop <dhwnduq12@naver.com>
2022-09-28 02:51:41 +0900

    Leetcode 10

--- String/10_Regular_Expression_Matching.py
+++ String/10_Regular_Expression_Matching.py
@@ -1,8 +1,6 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if len(s) > len(p):
-            return False
-        elif len(s) < len(p):
+            """
             ls = [False for _ in range(len(p))]
             result = []
             for i, item in enumerate(p):
@@ -11,3 +9,25 @@ class Solution:
                 else:
                     ls[i] = False
             return True
+            """
+            def recursion(ss: str, pp: str) -> bool:
+                if not pp and not ss:
+                    return True
+                if not pp:
+                    return False
+                pplen = len(pp)
+                sslen = len(ss)
+                if pplen > 1 and pp[1] == '*':
+                    if recursion(ss, pp[2:]) is True:
+                        return True
+                    if sslen > 0 and (ss[0] == pp[0] or pp[0] == '.'):
+                        return recursion(ss[1:], pp)
+                else:
+                    if sslen > 0 and (ss[0] == pp[0] or pp[0] == '.'):
+                        return recursion(ss[1:], pp[1:])
+                return False
+            return recursion(s, p)
+
+
+test = Solution()
+test.isMatch("ab", ".*c")
